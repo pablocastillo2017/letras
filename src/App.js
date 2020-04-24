3582e41245eb5f639609dc0a65fe9816fa200df9
Pablo Castillo <pablocastilloastudillo@gmail.com>
2020-04-24 15:51:33 -0400

    Fin de Proyecto 13

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Fragment, useState, useEffect } from "react";
 import Formulario from "./components/Formulario";
+import Cancion from "./components/Cancion";
+import Info from "../src/Info";
 import axios from "axios";
 
 function App() {
-  //Definir el State
-  const [busquedaletra, guardarBusquedaletra] = useState({});
-
-  // State para la letra
-
-  const [letra, guardarLetra] = useState({});
+  // definir el state
+  const [busquedaletra, guardarBusquedaLetra] = useState({});
+  const [letra, guardarLetra] = useState("");
+  const [info, guardarInfo] = useState({});
 
   useEffect(() => {
     //La primera vez busqueda letra va a estar vacio
@@ -18,18 +18,35 @@ function App() {
     const consultarApiLetra = async () => {
       // Extraer Propiedades de busquedaletra
       const { artista, cancion } = busquedaletra;
-
       const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
-      const resultado = await axios(url);
+      const url2 = `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artista}`;
 
-      guardarLetra(resultado.data.lyrics);
+      // para obtener las Propiedades de las 2 URLS se usa
+      // un PROMISE()
+      const [letra, informacion] = await Promise.all([axios(url), axios(url2)]);
+
+      guardarLetra(letra.data.lyrics);
+      guardarInfo(informacion.data.artists[0]);
+
+      // guardarLetra(resultado.data.lyrics);
     };
     consultarApiLetra();
-  }, [busquedaletra]);
+  }, [busquedaletra, info]);
 
   return (
     <Fragment>
-      <Formulario guardarBusquedaletra={guardarBusquedaletra} />
+      <Formulario guardarBusquedaLetra={guardarBusquedaLetra} />
+
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6">
+            <Info info={info} />
+          </div>
+          <div className="col-md-6">
+            <Cancion letra={letra} />
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 }
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,16 +1,15 @@
 import React, { useState } from "react";
 
-const Formulario = ({ guardarBusquedaletra }) => {
+const Formulario = ({ guardarBusquedaLetra }) => {
   const [busqueda, guardarBusqueda] = useState({
     artista: "",
     cancion: "",
   });
-
   const [error, guardarError] = useState(false);
 
   const { artista, cancion } = busqueda;
 
-  // Funcion a cada input para leer su contenido
+  // función a cada input para leer su contenido
   const actualizarState = (e) => {
     guardarBusqueda({
       ...busqueda,
@@ -18,18 +17,18 @@ const Formulario = ({ guardarBusquedaletra }) => {
     });
   };
 
-  // Consultar las APIS
-
+  // consultar las apis
   const buscarInformacion = (e) => {
     e.preventDefault();
+
     if (artista.trim() === "" || cancion.trim() === "") {
       guardarError(true);
       return;
     }
     guardarError(false);
+    // Todo bien, pasar al componente principal
 
-    //Pasar Al componente Principal
-    guardarBusquedaletra(busqueda);
+    guardarBusquedaLetra(busqueda);
   };
 
   return (
@@ -46,7 +45,8 @@ const Formulario = ({ guardarBusquedaletra }) => {
             className="col card text-white bg-transparent mb-5 pt-5 pb-2"
           >
             <fieldset>
-              <leyend className="text-center">Buscador Letras Canciones</leyend>
+              <legend className="text-center">Buscador Letras Canciones</legend>
+
               <div className="row">
                 <div className="col-md-6">
                   <div className="form-group">
@@ -75,7 +75,8 @@ const Formulario = ({ guardarBusquedaletra }) => {
                   </div>
                 </div>
               </div>
-              <button type="submit" btn="btn btn-primary float-right">
+
+              <button type="submit" className="btn btn-primary float-right">
                 Buscar
               </button>
             </fieldset>
